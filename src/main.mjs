e6b95d009d931d20daf6335e7b06796f3093fdb5
moefinley <moefinley@outlook.com>
2022-06-25 16:45:19 +0100

    including mjs files

---
+++ src/main.mjs
@@ -0,0 +1,16 @@
+import { Argument, program } from 'commander';
+import * as fs from "fs";
+import { killAll, run } from "./runner.mjs";
+export function start() {
+    program.addArgument(new Argument('Config', 'JSON file containing the config including the processes you want to run'));
+    program.parse();
+    let pathToConfig = program.args[0];
+    const config = JSON.parse(fs.readFileSync(pathToConfig).toString());
+    run(config).then(() => {
+        console.log(config.successMessage);
+    }).catch(() => {
+        console.log(config.errorMessage);
+        killAll();
+    });
+}
+//# sourceMappingURL=main.mjs.map
