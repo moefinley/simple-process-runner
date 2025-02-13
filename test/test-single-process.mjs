65f3c1503c4e8e85dd22c4e5471662d413877b76
neilt <tim.neil+copilot@lexisnexisrisk.com>
2025-02-13 21:44:14 +0000

    - Fixed `echo hello` issue - Fixed logging path in test string

--- test/test-single-process.mjs
+++ test/test-single-process.mjs
@@ -3,7 +3,6 @@ import {expectOutput} from "./expect-output.mjs";
 describe('A single process', function () {
     const expectedOutput = [
         'Starting process "Say hello" 1 run remaining',
-        'logging to C:\\Projects\\simple-process-runner-2\\test\\logs',
         'Say hello (0)>',
         'hello',
         'All processes completed successfully'
